3f69c0b6dc650ce010299e74d74da759df4b1285
PubHub <pubhub.work@gmail.com>
2025-06-30 00:58:28 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,8 +53,8 @@ export default function Hero() {
         <div className="text-center max-w-[90%] mx-auto px-6">
           {/* Hero Content */}
           <div className="space-y-6">
-            <h1 className="text-xl leading-[1.2] md:text-6xl md:leading-[1.2] lg:text-7xl lg:leading-[1.2] font-bold text-white w-full">
-              One platform to manage, share, and grow in public.
+            <h1 className="text-xl leading-[1.2] md:text-4xl md:leading-[1.2] lg:text-6xl lg:leading-[1.2] font-bold text-white w-full">
+              One platform to manage, <br/> share, and grow in public.
             </h1>
 
             <p className="text-xs md:text-lg text-green-100 leading-relaxed font-medium">
